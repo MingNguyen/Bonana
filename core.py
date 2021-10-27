423f63253167da59b3b315fe560d26ad5f28d02f
MingNguyen <91204964+MingNguyen@users.noreply.github.com>
2021-10-28 00:36:08 +0700

    Add files via upload

--- core.py
+++ core.py
@@ -1,16 +1,23 @@
+#import the library is used
 import numpy as np
 from math import cos,sin,pi,sqrt
 class banana():
+    
     def __init__(self):
-
+        # R1 is the distance between the circle center point to the center of the coordinate
         self.R1 = 2
+        # A, B is the rotating x and z axis 
         self.A, self.B = 0,0
+        # the pace to use for the movement of the angle
         self.A_PACE,self.B_PACE = 0.314,0.0785
         self.THETA_PACE,self.PHI_PACE = 0.04,0.06
-
+        # the depth and the circle center point is used for moving the object to a new virtual coordinate system
+        self.depth = np.array((0,0,6))
         self.circle_center_vector = np.array((self.R1, 0, 0))
-        self.surface_normal = []
+        # these to in 3D dimension which to memorise the frame, the point and that point cordinate
+        self.surface_normal_3D = []
         self.cordinate_3D = []
+        # these are the rotating matrix
         self.rotating_circle_matrix,self.x_rotating_banana,self.y_rotating_banana,self.move = (np.array(
             [[
                 [ 1, 0, 0],
@@ -18,42 +25,57 @@ class banana():
                 [ 0, 0, 1]
             ]] * 4
         ))
+        #after the next command, the program will go to line 112
         self.run()
 
     
-    def circle_point(self,new_R2):
-
-        circle_draw = np.array((new_R2 * cos(self.phi),new_R2 * sin(self.phi), 0))
-        self.normal_surface = np.array((cos(self.phi),sin(self.phi),0))
+    def circle_point(self,R2):
+        #update the cordinate and surface normal of the circle
+        circle_draw = np.array((R2 * cos(self.phi),R2 * sin(self.phi), 0))
+        self.circle_surface_normal = np.array((cos(self.phi),sin(self.phi),0))
+        #move the circle to it center
         self.circle = self.circle_center_vector + circle_draw
+        #update the value of the angle phi
         self.phi += self.PHI_PACE
 
-    def update_R2(self,R2):
-        if 2 >= R2 and R2 >1.7: return 0.21
-        elif 1.7 >= R2 and R2 > 1.5: return (0.798-0.42* R2)/0.4
-        elif 1.5 >= R2 and R2 > -1.4: return (2.676 - 0.16*R2)/5.8
-        elif -1.4 >= R2 and R2 >= -sqrt(3): return (-7/25 +sqrt(3) +0.8 *R2)/(sqrt(3)-1.4)/2
+    def update_R2(self,x):
+        #these function is to create value for r2
+        if 2 >= x and x >1.6: return 0.15
+        elif 1.6 >= x and x > 1.5: return (0.447-0.27* x)/0.1
+        elif 1.5 >= x and x > -1.4: return (2.676 - 0.16*x)/5.8
+        elif -1.4 >= x and x >= -sqrt(3): return (-7/25 +sqrt(3) +0.8 *x)/(sqrt(3)-1.4)/2
     
     def write_cordinate(self,frame):
-        self.cordinate_3D[frame].append(self.torus + np.array((0,0,5)))
-        self.surface_normal[frame].append(self.banana_normal_surface) 
+        # to write the cordinate to the generators which are the output
+        self.cordinate_3D[frame].append(self.banana + self.depth)
+        self.surface_normal_3D[frame].append(self.banana_surface_normal) 
 
     def draw_banana(self,frame):
+        # theta is the angle for "rotating circle matrix" to move the circle to a new cordinate
         self.theta = 0
+        # make the loops until all every circle in the frame are written
         while self.theta < 5*pi/6:
-            self.R2 = 2 * cos(self.theta)
-            new_R2 = self.update_R2(self.R2)
+            # R2 is the projection of the circle to the coordinate system
+            banana_point = self.R1 * cos(self.theta)
+            #go to line 39
+            R2 = self.update_R2(banana_point)
+            #update new value of theta after has been used 
             self.theta += self.THETA_PACE
-            
-            self.a = self.move @ self.rotating_circle_matrix
+            # temporary matrix is to contain all 3 rotation and its dot product
+            self.temporary = self.move @ self.rotating_circle_matrix
+            # phi is the angle for "circle matrix" to move the circle to a new cordinate
             self.phi = 0
+            # make the loops until a circle has been drawn
             while self.phi < 2*pi:
-                self.phi += self.PHI_PACE
-                self.circle_point(new_R2)
-                self.torus =  self.a @ self.circle
-                self.banana_normal_surface = self.a @ self.normal_surface
+                #function to draw the circle, go to line 30
+                self.circle_point(R2)
+                #make the dot products to get the banana cordinate and surface normal in a frame
+                self.banana =  self.temporary @ self.circle
+                self.banana_surface_normal = self.temporary @ self.circle_surface_normal
+                #this function is to save the output of core, go to line 46
                 self.write_cordinate(frame)
-            
+
+            #update the rotation matrixes
             self.rotating_circle_matrix = np.array(
                 (
                     (cos(self.theta), 0, sin(self.theta)),
@@ -63,8 +85,10 @@ class banana():
             )
         
     def new_frame(self):
+        #update the angle any time the function is called
         self.A += self.A_PACE
         self.B += self.B_PACE
+        #update the rotation matrixes
         self.x_rotating_banana = np.array(
                 (
                     (1, 0, 0),
@@ -79,14 +103,20 @@ class banana():
                     (0, 0, 1)
                 )
             )
+        #this is the dot product between 2 rotation matrixes
         self.move = self.x_rotating_banana @ self.z_rotating_banana
 
-
+    # This function is to start the drawing banana in 3D space
     def run(self):
+        # frame is a local value to index the movement of the banana
         frame = 0
+        # this command is to require the system to run all the frame can happen
         while self.B < 2*pi:
+            # 2 next commands require to make a new generator to install the value of a new frame
             self.cordinate_3D.append([])
-            self.surface_normal.append([])   
+            self.surface_normal_3D.append([])
+            # this command is to draw a banana and go to the line 51
             self.draw_banana(frame)
+            # this comment is to update some variables, go to line 85
             self.new_frame()
             frame += 1
--- screen.py
+++ screen.py
@@ -1,56 +1,80 @@
+#import some library 
 import numpy as np
 from math import cos,sin,pi
-from core import banana
 from os import system
 from time import sleep
-
+#import the banana class from core.py file
+from core import banana
 class show():
     def __init__(self,cordinate_3D,surface_normal):
+        #save the output in class variable
         self.surface_normal = surface_normal
-        self.shadow = '.,-~:;=!*#$@'
         self.cordinate_3D = cordinate_3D
+        # string that demonstrate the lightness
+        self.shadow = '.,-~:;=!*#$@'
+        # in this code, we set the screen in square as the default
         self.width = 65
-
-        self.K2 = 4
-        self.K1 = self.width*self.K2*3/(8*3)
-        self.light_source = np.array((0,1,-1))
+        # the distance from the watcher to the screen
+        self.K1 = 40
+        # the luminance direction.
+        self.light_direction = np.array((0,1,-1))
+        # z-buffer is to contain the value of ooz, it has 3Dspace
         self.zbuffer = [[[0 for i in range(self.width)] for j in range(self.width)] for k in range(len(self.cordinate_3D))]
+        # our output needs to be written
         self.output = [[[' ' for i in range(self.width)] for j in range(self.width)] for k in range(len(self.cordinate_3D))]
-
+        #goto line 27
         self.protection()
 
     def protection(self):
+        # to scan all frame
         for i in range(len(self.cordinate_3D)):
+            #to scan all point
             for j in range(len(self.cordinate_3D[0])):
+                # these are the coordinate number
                 x,y,z = self.cordinate_3D[i][j]
+                # ooz is used to compare the depth of 2 point have a same x and y
                 ooz = 1/z
+                # we caculate the location of projection on screen 
                 xp = int(self.width/2 + self.K1*ooz*x)
-                yp = int(self.width/2 - self.K1*ooz*y)
-                L = self.surface_normal[i][j][1] - surface_normal[i][j][2]
-                if L > 0:
-                    if ooz > self.zbuffer[i][xp][yp]:
-                        l_index = int(L*8)
-                        self.zbuffer[i][xp][yp] = ooz
-                        self.output[i][xp][yp] = self.shadow[l_index]
+                # the equation is minus because the upper y in the coordinate system is the lower b in the screen show
+                yp = int(self.width/2  - self.K1*ooz*y)
+                #calculate the luminance
+                luminance = self.surface_normal[i][j] @self.light_direction
+                #if the point have the luminance higher than 0 and the if it have same x,y to the previous point,
+                #mke sure that only show which is mostly shining
+                if luminance > 0 and ooz > self.zbuffer[i][xp][yp]:
+                    #max of luminance is sqrt(2) so the max value of l index = 12 = len(shadow)
+                    l_index = int(luminance*8)
+                    #update the zbuffer
+                    self.zbuffer[i][xp][yp] = ooz
+                    #update the output with the shadow
+                    self.output[i][xp][yp] = self.shadow[l_index]
 
 action = banana()
+# We take the output of the banana
 cordinate = action.cordinate_3D
-surface_normal = action.surface_normal
+surface_normal = action.surface_normal_3D
+# to reduce ram memories
 del action
+#output the banana is input of show
 alo = show(cordinate,surface_normal)
-
+# we only take the  output of show
+a = alo.output
+# to reduce ram memories
+del alo
+# these line are to transfer the output from the coordinate form to the string form
 h = ['']
-for i in range(len(alo.output)):
-    for j in range(len(alo.output[0])):
-        for k in range(len(alo.output[0][0])):
-            h[i] += alo.output[i][j][k]
+for i in range(len(a)):
+    for j in range(len(a[0])):
+        for k in range(len(a[0][0])):
+            h[i] += a[i][j][k]
         h[i] += '\n'
     h.append('')
 i = 0
+#this line is to show it on the screen
 while True:
-    if i < len(alo.output):
+    if i < len(a):
         system("cls")
-        print('\n\n\n\n\n')
         print(h[i])
         sleep(0.07)
         i += 1
